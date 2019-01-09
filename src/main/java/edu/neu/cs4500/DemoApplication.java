8fc341501a255bd033ec5cbd612f21c0c1e2a9ee
ysmolyar <yonismolyar1@gmail.com>
2019-01-09 11:46:45 -0500

    first commit

---
+++ src/main/java/edu/neu/cs4500/DemoApplication.java
@@ -0,0 +1,14 @@
+package edu.neu.cs4500;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+
