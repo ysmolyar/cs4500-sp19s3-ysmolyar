b879cb3313b19b368ed83a12ebf02dd5225c4ac7
ysmolyar <yonismolyar1@gmail.com>
2019-01-22 06:14:06 -0500

    created method findUserById in user service

--- src/main/java/edu/neu/cs4500/services/UserService.java
+++ src/main/java/edu/neu/cs4500/services/UserService.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import edu.neu.cs4500.models.User;
@@ -18,5 +19,14 @@ public class UserService {
 	@GetMapping("/api/user")
 	public List<User> findAllUsers() {
 		return users;
+	}
+	@GetMapping("/api/user/{userId}")
+	public User findUserById(
+		@PathVariable("userId") Integer id) {
+		for(User user: users) {
+			if(user.getId() == id) {
+				return user;
+		}
+		return null;
 	}
 }
